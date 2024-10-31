b34e58616af9f85841946929fbd3ab2ddd74c951
Sommar-skog <amandaaronsson@hotmail.com>
2024-10-31 14:47:20 +0100

    GB-10-AA Gjort klart klassen. Skapat-close metod Gjort test för att test med kommunication mellan server och klient fungerar.

--- src/CommunicationHandler.java
+++ src/CommunicationHandler.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class CommunicationHandler {
 
-    private String name;
+    private String name; // kan skrivas ut på skärmen över kartan
     private String host;
     private int port;
     private Socket clientSocket;
@@ -30,22 +30,26 @@ public class CommunicationHandler {
 
     //BG-10-AA
     private void connectToServer(){
-        try (Socket socket = new Socket(this.host,this.port)) {
-            this.clientSocket = socket;
-            System.out.println("Connected to  server");
+        //Try-with-resources om vi lägger in communication här i blocket. Socket stängs då i slutet av blocket.
+        // Annars använd closeSocket()- metod.
+        try  {
+            clientSocket = new Socket(this.host,this.port);
+            System.out.println("Connected to server");
             this.writer = new PrintWriter (new OutputStreamWriter(clientSocket.getOutputStream()),true);
             this.reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         } catch (Exception e) {
-            System.out.println("Could not add socket");
+            System.out.println("Could not connect to server at " + this.host + ":" + this.port);
             e.printStackTrace();
         }
     }
 
     //BG-10-AA
     private void connectToClient(){
-        try (ServerSocket serverSocket1 = new ServerSocket(this.port)){
-            this.serverSocket = serverSocket1;
+        //Try-with-resources om vi lägger in communication här i blocket. Socket stängs då i slutet av blocket.
+        // Annars använd closeSocket()- metod.
+        try {
+            serverSocket = new ServerSocket(this.port);
             System.out.println("Waiting for client to connect");
             clientSocket = serverSocket.accept();
             System.out.println("Client connected");
@@ -54,29 +58,123 @@ public class CommunicationHandler {
             this.reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         } catch (Exception e) {
-            System.out.println("Could not add serverSocket");
+            System.out.println("Could not start server on port " + this.port );
             e.printStackTrace();
         }
     }
 
     //GB-10-AA
-    private void handleIncomingMessages (){ // vet ännu inte om private eller public
+    public void handleIncomingMessages (){
         String incomingMessage;
         try{
             while ((incomingMessage = reader.readLine()) != null){
+                System.out.println("Mottaget: " + incomingMessage); // ska nog göras om och flyttas till grafisk vy
                 //Uppdatera spel, vet ej om metoden ska ligga här eller på annan plats
             }
 
         } catch (IOException e){
-            System.out.println("Connection closed by server");
+            System.out.println("Connection closed by other side");
         }
     }
 
     //GB-10-AA
-    private void handleSendingMessages(String message){
+    public void handleSendingMessages(String message){
         if (message != null && !message.trim().isEmpty()){
             writer.println(message);
-            //Uppdatera egen spelplan (vet ej om metoden ska ligga här eller på annan plats
+            //Uppdatera egen spelplan (vet ej om metoden ska ligga här eller på annan plats)
         }
+    }
+
+    //GB-10-AA
+    public void closeSocket() {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+            if (writer != null){
+                writer.close();
+            }
+            if (clientSocket != null && !clientSocket.isClosed()){
+                clientSocket.close();
+            }
+            if (serverSocket != null && !clientSocket.isClosed()){
+                serverSocket.close();
+            }
+        } catch (IOException e){
+            System.out.println("Error closing resources");
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
+
+    public void setClientSocket(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public void setServerSocket(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public BufferedReader getReader() {
+        return reader;
+    }
+
+    public void setReader(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public PrintWriter getWriter() {
+        return writer;
+    }
+
+    public void setWriter(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public String toString() {
+        return "CommunicationHandler{" +
+                "name='" + name + '\'' +
+                ", host='" + host + '\'' +
+                ", port=" + port +
+                ", clientSocket=" + clientSocket +
+                ", serverSocket=" + serverSocket +
+                ", reader=" + reader +
+                ", writer=" + writer +
+                '}';
     }
 }
