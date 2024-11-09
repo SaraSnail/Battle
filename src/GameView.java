ee4e2faeb8b58e2755e4a8ff2c9519996ff74b5a
a-westerberg <alexander.westerberg.svedberg@gmail.com>
2024-11-09 15:56:25 +0100

    AWS GB-14 försöker få till en on mouse funktion som visar vilken cell, går inte super bra.

--- src/GameView.java
+++ src/GameView.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -20,25 +19,32 @@ public class GameView extends Application {
         enemyGameBoard= new GameBoard();
 
         AnchorPane myGame = new AnchorPane();        // Min Spelplan
-        battleGroundFX(myGame, myGameBoard, false);
-        myGame.setLayoutX(50);
-        myGame.setLayoutY(50);
+        AnchorPane enemyGame = new AnchorPane();    // Motståndare Spelplan
 
         Label myLabel = new Label("Min Spelplan");
         myLabel.setLayoutX(50);
         myLabel.setLayoutY(25);
 
-        AnchorPane enemyGame = new AnchorPane();    // Motståndare Spelplan
-        battleGroundFX(enemyGame, enemyGameBoard, true);
-        enemyGame.setLayoutX(600);
-        enemyGame.setLayoutY(50);
-
         Label enemyLabel = new Label("Motståndarens Spelplan");
         enemyLabel.setLayoutX(600);
         enemyLabel.setLayoutY(25);
 
+        Label coordinateLabel = new Label();
+        coordinateLabel.setStyle("/*-fx-background-color: transparant;*/ -fx-border-color: black;");
+        coordinateLabel.setVisible(false);
+
+
+
+        battleGroundFX(myGame, myGameBoard, false, coordinateLabel);
+        battleGroundFX(enemyGame, enemyGameBoard, true,coordinateLabel );
+
+        myGame.setLayoutX(50);
+        myGame.setLayoutY(50);
+        enemyGame.setLayoutX(600);
+        enemyGame.setLayoutY(50);
+
         AnchorPane stack = new AnchorPane();
-        stack.getChildren().addAll(myLabel,enemyLabel,myGame,enemyGame);
+        stack.getChildren().addAll(myLabel,enemyLabel,myGame,enemyGame,coordinateLabel);
 
         Scene scene = new Scene(stack, 1600, 600);
         primaryStage.setScene(scene);
@@ -50,8 +56,10 @@ public class GameView extends Application {
     }
 
     //aws
-    public void battleGroundFX(AnchorPane boardPane, GameBoard board, boolean isEnemy) {
+    public void battleGroundFX(AnchorPane boardPane, GameBoard board, boolean isEnemy, Label cooerdinateLabel) {
         char[][] gameBoardFX = board.getBoard();
+
+        /*Tooltip tooltip = new Tooltip();*/
 
         for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
@@ -69,6 +77,30 @@ public class GameView extends Application {
                     }
                 }
                 cell.setStroke(Color.BLACK);
+
+                String coordinate = String.valueOf((char) ('A' + r)) +(c + 1);
+                cell.setOnMouseEntered(e -> {
+                    double centerX = cell.getX() + cell.getWidth() / 2;
+                    double centerY = cell.getY() + cell.getHeight() / 2;
+
+                    cooerdinateLabel.setText(coordinate);
+                    cooerdinateLabel.setVisible(true);
+
+                    /*double width = cooerdinateLabel.getWidth();
+                    double height = cooerdinateLabel.getHeight();*/
+
+                    cooerdinateLabel.setTranslateX(centerX - cooerdinateLabel.getWidth() / 2);
+                    cooerdinateLabel.setTranslateY(centerY - cooerdinateLabel.getHeight() / 2);
+
+
+                });
+
+                cell.setOnMouseExited(e -> {cooerdinateLabel.setVisible(false);});
+
+
+
+
+
                 boardPane.getChildren().add(cell);
             }
         }
