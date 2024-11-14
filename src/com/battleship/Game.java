641dbc6d3cf19fa7059c1af425b5d2552caee595
Sara <sara.andersson185@gmail.com>
2024-11-14 09:28:10 +0100

    Klar med vad jag kan göra med checkIfGameOver()

--- src/com/battleship/Game.java
+++ src/com/battleship/Game.java
@@ -206,6 +206,7 @@ public class Game {
         //GB-33-SA
         if (message.equalsIgnoreCase("game over")) {
             gameOver = true;
+
             //updateMaps(lastShot, enemyGameBoard);//Uppdaterar GUI också
             // Får game over från motståndaren och uppdaterar deras karta så sista skottet på dem syns
             //lastShot fixa
